f8c6ad8c3ffdd057e79e2c84d70ad84e046f8df1
Maksim Dvoinishnikov <maksim_dvoinishnikov@epam.com>
2023-09-09 12:13:16 +0400

    Setup deploy 🚀

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions'
 
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
