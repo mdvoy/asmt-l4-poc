3edd2e5477869094340cd3979768da21686896a0
Maksim Dvoinishnikov <maksim_dvoinishnikov@epam.com>
2023-09-09 12:59:07 +0400

    Change netlify function 💫

--- netlify/functions/hello.ts
+++ netlify/functions/hello.ts
@@ -1,10 +1,40 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions'
 
+const API_URL = 'https://www.epam.com/services/vacancy/search'
+
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({ message: 'Hello World' }),
+    const queryParams = {
+        locale: 'en',
+        limit: '1000',
+        recruitingUrl: '/content/epam/en/careers/job-listings/job',
+        sort: 'relevance',
+        offset: '0',
+    }
+    const queryString = new URLSearchParams(queryParams).toString()
+
+    const fullUrl = `${API_URL}?${queryString}`
+
+    const fetchOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }
+
+    try {
+        const response = await fetch(fullUrl, fetchOptions)
+        const data = await response.json()
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        }
+    } catch (error) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error }),
+        }
     }
 }
 
